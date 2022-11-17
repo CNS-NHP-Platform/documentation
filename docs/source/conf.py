51136bd98d2e5f9cbd5bba9bfdf8f304d6287e74
Dan Gale <danjgale@gmail.com>
2022-11-17 10:29:12 -0700

    initialized

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CNS NHP Platform'
+copyright = '2022, Dan Gale'
+author = 'Dan Gale'
 
 release = '0.1'
 version = '0.1.0'
